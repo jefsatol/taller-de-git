98d896a2317c1679b410ec1cdf2dcc947342ff4e
Jefferson <jefsatol@gmail.com>
2024-01-21 11:48:59 +0100

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,6 @@
 <?php
 // Autor: Jefferson Sánchez <jefsatol@gmail.com>
-// El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
 ?>
